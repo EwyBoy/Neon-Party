e3e21ed99c81a7ac3a845d416430715ca9cf55b5
EwyBoy <ewyboy@gmail.com>
2018-09-08 17:35:30 +0200

    v1.0.1

--- script.js
+++ script.js
@@ -1,53 +1,62 @@
-setInterval(function(){ draw() }, 25);
-setInterval(function(){ cycleMode(getRandomInt(0, 9), getRandomInt(0,9), getRandomInt(0,9)) }, 25);
+/* Runs the main function 40 (1000 / 25 = 40) timers per second */
+setInterval(function(){ main() }, 25);
 
-function draw() {
-    c = document.getElementById("myCanvas");
-    ctx = c.getContext("2d");
+/* Runs the cycleMode function 40 (1000 / 25 = 40) timers per second and gives it 3 random number between 0 and 9 */
+setInterval(function(){ cycleMode(getRandomIntInRange(0, 9), getRandomIntInRange(0, 9), getRandomIntInRange(0, 9)) }, 25);
 
-    var probabilityPercentage = 10;
-    var prob = probabilityPercentage * 0.01;
+var canvas, draw;
 
+/* Initializes the fields and runs the flash function  */
+function main() {
+    canvas = document.getElementById("myCanvas");
+    draw = canvas.getContext("2d");
     flash();
 }
 
+/* Cycles the different modes : if the random number between 0 - 9 is less then 2 it uses that mode */
+/* All three modes can be combined by random chance in various ways */
 function cycleMode(prob1, prob2, prob3) {
-    if (prob1 < 2)  vertical(c.width, c.height);
-    if (prob2 < 2)  horisontal(c.width, c.height);
-    if (prob3 < 2)  syre();
-
+    if (prob1 < 2)  vertical(canvas.width, canvas.height);
+    if (prob2 < 2)  horizontal(canvas.width, canvas.height);
+    if (prob3 < 2)  acid();
 }
 
-function horisontal(width, height) {
-    ctx.fillStyle = getRandomColor(getRandomInt(0,9));
+/* Draws horizontal lines in random colors */
+function horizontal(width, height) {
+    draw.fillStyle = getRandomColor(getRandomIntInRange(0, 9));
     for (var x = 0; x < width; x+= 20) {
-        ctx.fillRect(x, 0, 10, height);
+        draw.fillRect(x, 0, 10, height);
     }
 }
 
+/* Draws vertical lines in random colors */
 function vertical(width, height) {
-    ctx.fillStyle = getRandomColor(getRandomInt(0,9));
+    draw.fillStyle = getRandomColor(getRandomIntInRange(0, 9));
     for (var y = 0; y < height; y+= 20) {
-        ctx.fillRect(0, y, width, 10);
+        draw.fillRect(0, y, width, 10);
     }
 }
 
-function syre() {
-    ctx.fillStyle = getRandomColor(getRandomInt(0,9));
-    for (var x = 0; x < c.width; x += 15) {
-        for (var y = 0; y < c.height; y += 15) {
-            ctx.fillStyle = getRandomColor(getRandomInt(0,9));
-            ctx.fillRect(x, y, 10, 10)
+/* Draws a checkerboard pattern of squares in random colors */
+function acid() {
+    draw.fillStyle = getRandomColor(getRandomIntInRange(0, 9));
+    for (var x = 0; x < canvas.width; x += 15) {
+        for (var y = 0; y < canvas.height; y += 15) {
+            draw.fillStyle = getRandomColor(getRandomIntInRange(0, 9));
+            draw.fillRect(x, y, 10, 10)
         }
     }
 }
 
+/* Creates the background color of the screen in a random color - This one has a 100% chance to trigger unlike the other shapes */
 function flash() {
-    ctx.fillStyle = getRandomColor(getRandomInt(0,9));
-    ctx.fillRect(0, 0, c.width, c.height);
+    draw.fillStyle = getRandomColor(getRandomIntInRange(0, 9));
+    draw.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function getRandomInt(min, max) {
+
+/* Generates a random number between min & max including both of em */
+function getRandomIntInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
